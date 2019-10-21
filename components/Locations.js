5da4a8cd97f6d72bd6a8b578a06ed295cd951226
James Tang <jytang10@gmail.com>
2019-10-20 18:47:55 -0700

    Imported redux methods to Locations

--- components/Locations.js
+++ components/Locations.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image, Button, ActivityIndicator } from 'react-native';
+import { getLocations, deleteLocation } from '../actions';
+import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import _ from 'lodash';
 
 class Locations extends Component {
+
+  componentDidMount(){
+    // this.props.getLocations();
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -18,8 +27,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'orange',
+    backgroundColor: '#fff',
+    padding: 10
   },
 });
 
-export default Locations;
+function mapStateToProps(state){
+  const listOfLocations = _.map(state.locationsList, (val, key) => {
+    return {
+      ...val,
+      key: key
+    }
+  })
+  return {
+    listOfLocations,
+    loadingReducer: state.loadingReducer.loadingReducer
+  }
+}
+
+export default connect(mapStateToProps, {getLocations, deleteLocation})(Locations);
