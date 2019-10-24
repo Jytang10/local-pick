bdc6cdee0b6af8ee98b7197c54fc557165f0ab00
James Tang <jytang10@gmail.com>
2019-10-23 19:35:52 -0700

    Added update functionality to UpdateNote

--- components/UpdateNote.js
+++ components/UpdateNote.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { updateNote } from '../actions';
+import { connect } from 'react-redux';
 
 class UpdateNote extends Component {
+  state = {
+    content: this.props.navigation.state.params.content,
+    key: this.props.navigation.state.params.key
+  }
+
+  submitUpdate = () => {
+    const { content, key } = this.state;
+    this.props.updateNote(content, key);
+    this.setState({
+      content: "",
+      key: ""
+    })
+    this.props.navigation.navigate('LocationDetails');
+  }
+
   render() {
+    const { content } = this.state;
     return (
       <View style={styles.container}>
-        <Text>UpdateNote</Text>
+        <Text>Update Note Screen</Text>
+        <TextInput
+          style={styles.contentInput}
+          placeholder="content"
+          onChangeText={content => this.setState({ content })}
+          value={content}>
+        </TextInput>
+        <Button title="Submit" onPress={this.submitUpdate}></Button>
       </View>
     );
   }
@@ -18,6 +43,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#2c3e50',
   },
+  contentInput: {
+    marginTop: 20,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
 
-export default UpdateNote;
+export default connect(null, {updateNote})(UpdateNote);
