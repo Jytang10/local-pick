f01d3ae929ca70ed93341db1b2191842e15ff884
James Tang <jytang10@gmail.com>
2019-10-29 21:56:45 -0700

    Added icon, title and description to Home

--- components/Home.js
+++ components/Home.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-
+import { MaterialIcons } from '@expo/vector-icons';
 class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Search')}>
-          <Text>Guest User click here</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
-          <Text>Click here to Login or Sign Up</Text>
-        </TouchableOpacity>
+        <View style={styles.mainContainer}>
+          <MaterialIcons name="local-dining" color="white" raised size={100}/>        
+          <Text style={styles.title}>Local Pick</Text>
+          <Text style={styles.description}>A community to share and discover local food recommendations.</Text>
+        </View>
+        <View style={styles.navContainer}>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('Search')}>
+            <Text>Guest User click here</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
+            <Text>Click here to Login or Sign Up</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -21,8 +28,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: 'purple',
   },
+  mainContainer: {
+    alignItems: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    color: 'white'
+  },
+  description: {
+    color: 'white'
+  }
 });
 
 export default Home;
